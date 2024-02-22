330dbd2f8c39e43a7b2b8e63494c5572f918d800
Greg PFISTER <gregory.pfister@gmail.com>
2024-02-22 18:09:26 +0100

    Remove SwiftData

--- iOS/App/Views/GPWApp.swift
+++ iOS/App/Views/GPWApp.swift
@@ -27,24 +27,24 @@ import SwiftUI
 struct GPWApp: App {
     @UIApplicationDelegateAdaptor(GPWAppDelegate.self) var delegate
 
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            GPWItem.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+//    var sharedModelContainer: ModelContainer = {
+//        let schema = Schema([
+//            GPWItem.self,
+//        ])
+//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+//
+//        do {
+//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+//        } catch {
+//            fatalError("Could not create ModelContainer: \(error)")
+//        }
+//    }()
 
     var body: some Scene {
         WindowGroup {
             GPWContentView()
                 .environment(\.font, .gpwBody)
         }
-        .modelContainer(sharedModelContainer)
+//        .modelContainer(sharedModelContainer)
     }
 }
